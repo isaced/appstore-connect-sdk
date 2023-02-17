98dfaae7d4916c8cb6ce4a63c5c9e68176e90771
isaced <isaced@163.com>
2023-02-18 00:49:52 +0800

    Update tests

--- test/events.test.ts
+++ test/events.test.ts
@@ -8,23 +8,22 @@ import AppStoreConnectAPI from "../src/main";
 
 dotenv.config();
 
-test("Get In-App Events List", async () => {
-  const client = new AppStoreConnectAPI({
-    issuerId: process.env.ISSUER_ID!,
-    privateKeyId: process.env.PRIVATE_KEY_ID!,
-    privateKey: loadP8File(),
-    fetchApi: fetch as FetchAPI,
-  });
+// Create a new instance of the AppStoreConnectAPI client with the required parameters.
+const client = new AppStoreConnectAPI({
+  issuerId: process.env.ISSUER_ID!,
+  privateKeyId: process.env.PRIVATE_KEY_ID!,
+  privateKey: loadP8File(),
+  fetchApi: fetch as unknown as FetchAPI,
+});
 
+// Test case to get a list of apps.
+test("Get app list", async () => {
   const res = await client.call(AppsApi).appsGetCollection();
   console.log(res);
   expect(res).toBeDefined;
 });
 
+// Function to load the private key from the file system.
 export function loadP8File() {
-  const FILE_PATH = process.env.PRIVATE_KEY_PATH || process.env.P8_FILE!;
-  const fileContent = fs.readFileSync(FILE_PATH);
-  const privateKey = fileContent.toString();
-  console.log(privateKey);
-  return privateKey;
+  return fs.readFileSync(process.env.P8_FILE!).toString();
 }
