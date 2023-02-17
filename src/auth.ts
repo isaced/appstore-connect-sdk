8d3145a1c9594d792ceca3d03d3e0f7932cb3566
isaced <isaced@163.com>
2023-02-18 00:48:58 +0800

    Add comments to the core code

--- src/auth.ts
+++ src/auth.ts
@@ -1,25 +1,41 @@
 import jwt from "jsonwebtoken";
 
+// Options for generating the JWT token
 interface GenerateAuthTokenOptions {
+  // The ID of the private key to use
   apiKeyId: string;
+  // The ID of the issuer (i.e., your App Store Connect account)
   issuerId: string;
+  // The private key used to sign the token
   privateKey: string;
+  // The time (in seconds) until the token expires (default 10 minutes)
   expirationTime?: number;
 }
 
+// Generates a JWT token for use with the App Store Connect API
 export function generateAuthToken({ apiKeyId, issuerId, privateKey, expirationTime }: GenerateAuthTokenOptions) {
+  // Default expiration time is 10 minutes (600 seconds)
   const EXPIRATION_TIME = expirationTime || 10 * 60;
+
+  // Generate the token using the provided options
   const token = jwt.sign({}, privateKey, {
     algorithm: "ES256",
     expiresIn: EXPIRATION_TIME,
     issuer: issuerId,
     audience: "appstoreconnect-v1",
     header: {
+      // The algorithm used to sign the token (ECDSA with SHA-256)
       alg: "ES256",
+      // The ID of the private key used to sign the token
       kid: apiKeyId,
+      // The type of the token (JWT)
       typ: "JWT",
     },
   });
-  console.log("Gen JWT Token: ", token);
+
+  // Log the generated token to the console for debugging purposes
+  console.log("Generated JWT Token: ", token);
+
+  // Return the generated token
   return token;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,58 @@ import { FetchAPI, BaseAPI } from "./openapi/runtime";
 import { Configuration } from "./openapi";
 import { generateAuthToken } from "./auth";
 
+/**
+ * Options for configuring the AppStoreConnectAPI instance.
+ */
 interface AppStoreConnectAPIOptions {
+  /**
+   * The issuer ID associated with the private key.
+   */
   issuerId: string;
+
+  /**
+   * The ID of the private key.
+   */
   privateKeyId: string;
+
+  /**
+   * The private key in PEM format.
+   */
   privateKey: string;
+
+  /**
+   * An optional FetchAPI instance to use for making HTTP requests.
+   */
   fetchApi?: FetchAPI;
 }
 
+/**
+ * The App Store Connect SDK for Node.js is written in TypeScript and supports all APIs
+ * based on OpenAPI Generator.
+ * 
+ * https://github.com/isaced/appstore-connect-sdk
+ */
 export default class AppStoreConnectAPI {
   private bearerToken?: string;
   public configuration?: Configuration;
 
+  /**
+   * Creates an instance of the AppStoreConnectAPI.
+   * @param options - The configuration options for the API.
+   * @param options.issuerId - The issuer ID for generating JWT token.
+   * @param options.privateKeyId - The ID of the private key used for generating JWT token.
+   * @param options.privateKey - The content of the private key used for generating JWT token.
+   * @param options.fetchApi - (Optional) The FetchAPI implementation to use for API requests.
+   */
   constructor(options: AppStoreConnectAPIOptions) {
+    // Generate an authentication token using the provided options.
     this.bearerToken = generateAuthToken({
       apiKeyId: options.privateKeyId,
       issuerId: options.issuerId,
       privateKey: options.privateKey,
     });
 
+    // Create a Configuration object with the authentication token and any provided FetchAPI implementation.
     this.configuration = new Configuration({
       headers: {
         Authorization: `Bearer ${this.bearerToken}`,
@@ -28,6 +62,11 @@ export default class AppStoreConnectAPI {
     });
   }
 
+  /**
+  * Calls the specified API class using the current Configuration object.
+  * @param apiClass - The API class to instantiate.
+  * @returns An instance of the specified API class.
+  */
   call<T extends BaseAPI>(apiClass: new (configuration?: Configuration) => T): T {
     return new apiClass(this.configuration);
   }
