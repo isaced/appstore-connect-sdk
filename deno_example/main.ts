4447fbc8a7ca612e080115dd3cf97f7d79c0e10b
isaced <isaced@163.com>
2023-07-10 10:52:16 +0800

    chore: update deno_example

--- deno_example/main.ts
+++ deno_example/main.ts
@@ -1,15 +1,15 @@
-import "https://deno.land/std@0.177.0/dotenv/load.ts";
-import AppStoreConnectAPI from "npm:appstore-connect-sdk@0.0.5";
-import { AppsApi } from "npm:appstore-connect-sdk@0.0.5/dist/openapi/apis/index.js";
+import "https://deno.land/std@0.193.0/dotenv/load.ts";
+import AppStoreConnectAPI from "https://esm.sh/appstore-connect-sdk@1.0.0-beta.0";
+import { AppsApi } from "https://esm.sh/appstore-connect-sdk@1.0.0-beta.0/dist/openapi/apis/index.js";
 
 // Create a new instance of the AppStoreConnectAPI client with the required parameters.
-const client = new AppStoreConnectAPI.default({
-    // issuerId: Deno.env.get('ISSUER_ID')!,
-    // privateKeyId: Deno.env.get('PRIVATE_KEY_ID')!,
-    // privateKey: Deno.env.get('PRIVATE_KEY')!,
-    bearerToken: Deno.env.get('BEARER_TOKEN')!,
+const client = new AppStoreConnectAPI({
+  issuerId: Deno.env.get("ISSUER_ID")!,
+  privateKeyId: Deno.env.get("PRIVATE_KEY_ID")!,
+  privateKey: Deno.env.get("PRIVATE_KEY")!,
+  // bearerToken: Deno.env.get("BEARER_TOKEN")!,
 });
 
 // Test case to get a list of apps.
-const res = await client.call(AppsApi).appsGetCollection();
+const res = await (await client.call(AppsApi)).appsGetCollection();
 console.log(res);
